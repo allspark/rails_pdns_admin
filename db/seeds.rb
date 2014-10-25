1565f167d1fa3f9aa981139727faf6af61a52d29
Dennis Boerm <allspark@wormhole.eu>
2014-10-25 19:17:19 +0200

    roles seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+global_admin_role = Role.create(name: 'Sys-Admin');
+domain_owner_role = Role.create(name: 'Domain-Owner');
+record_owner_role = Role.create(name: 'Record-Owner');
+record_updater_role = Role.create(name: 'Record-Updater');
+
+
 admin_user = User.create(email: 'allspark@wormhole.eu', password: '1234567890', confirmed_at: Time.now)
