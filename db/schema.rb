2e2084547bc0c05b452a0ac7aecf998ddd37a687
Dennis Boerm <allspark@wormhole.eu>
2014-10-25 17:29:35 +0200

    cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025125552) do
+ActiveRecord::Schema.define(version: 20141025152804) do
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
