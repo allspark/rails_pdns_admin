0053f68fe0be37032c66f0512877b34206a5bfa1
Dennis Boerm <allspark@wormhole.eu>
2014-10-30 22:17:04 +0100

    system role and fix mapping

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,4 @@
 class Role < ActiveRecord::Base
-  enum title: [ :guest, :sysadmin, :domainown, :recordown, :recordupd, :dnsadmin, :user ]
+  enum title: { system: 0, guest: 1, sysadmin: 2, domainown: 3, recordown: 4, recordupd: 5, dnsadmin: 6, user: 7 }
 
 end
