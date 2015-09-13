0e82bf017c9030e38743dd2605cbe53b11c21797
Dennis Boerm <allspark@wormhole.eu>
2015-09-13 18:32:55 +0200

    added LICENSE, and readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# README
+
+currently in development under beta status
+
+## ruby version
+
+Developed with Ruby Version 2.1.4 and MySQL 5.5
+
+## Todo
+
+* write changelog
+* set version
+* implement RDNS (PTR) helper
+* complete feature and todo list
+
+## features
+
+* normal record creation, update, ...
+
+## Contact
+
+* E-Mail: allspark@wormhole.eu
+* Jabber/XMPP: allspark@wormhole.eu
--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,3 +1,20 @@
+# This file is part of Rails PowerDNS Admin
+#
+# Copyright (C) 2014-2015  Dennis <allspark> Börm
+#
+# Rails PowerDNS Admin is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+# Rails PowerDNS Admin is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Rails PowerDNS Admin.  If not, see <http://www.gnu.org/licenses/>.
+
 class UserRole < ActiveRecord::Base
   belongs_to :user
   belongs_to :role
